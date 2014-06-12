d94ed3d29130a5705b6c066103c02c3c8d7e27f8
Ray Cheung <ray.swc@gmail.com>
2014-06-12 23:58:15 +0800

    make fish more compatible

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ set fish_theme krisleech
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins brew rbenv bundler rails
+set fish_plugins brew rbenv bundler ndenv
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
@@ -16,5 +16,14 @@ set fish_plugins brew rbenv bundler rails
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
-set -x PGDATA /usr/local/var/postgres
+set -x EDITOR (which vim)
+
+# for Android
 set -x ANDROID_HOME /usr/local/opt/android-sdk
+launchctl setenv ANDROID_HOME $ANDROID_HOME
+set -x JAVA_HOME (/usr/libexec/java_home)
+launchctl setenv JAVA_HOME $JAVA_HOME
+
+set -x PATH (npm bin) $PATH
+
+set -x PGDATA /usr/local/var/postgres
