7bc6559096ccd5dd668c7db220355801cf80015e
Ray Cheung <ray.swc@gmail.com>
2014-06-12 13:51:32 +0800

    dropped rake plugin in oh-my-fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ set fish_theme krisleech
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins brew rbenv bundler rake rails
+set fish_plugins brew rbenv bundler rails
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
