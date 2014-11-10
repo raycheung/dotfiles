5ffd2aaea24be6f8779749a0d49040833dfea014
Ray Cheung <ray.swc@gmail.com>
2014-11-10 10:48:03 +0800

    Make it more clear

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,7 +9,7 @@ set fish_theme cor
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins brew rbenv bundler ndenv emoji-clock
+set fish_plugins brew rbenv bundler emoji-clock
 
 # Path to your custom folder (default path is $FISH/custom)
 set fish_custom ~/.dotfiles/oh-my-fish
@@ -20,14 +20,14 @@ set fish_custom ~/.dotfiles/oh-my-fish
 # I love vim
 set -x EDITOR (which vim)
 
-# for Android
-set -x ANDROID_HOME /usr/local/opt/android-sdk
-launchctl setenv ANDROID_HOME $ANDROID_HOME
-set -x JAVA_HOME (/usr/libexec/java_home)
-launchctl setenv JAVA_HOME $JAVA_HOME
-
 # for posgres
 set -x PGDATA /usr/local/var/postgres
 
+# for Android
+#set -x ANDROID_HOME /usr/local/opt/android-sdk
+#launchctl setenv ANDROID_HOME $ANDROID_HOME
+#set -x JAVA_HOME (/usr/libexec/java_home)
+#launchctl setenv JAVA_HOME $JAVA_HOME
+
 # for Python
-set -x PATH $HOME/Library/Python/2.7/bin $PATH
+#set -x PATH $HOME/Library/Python/2.7/bin $PATH
