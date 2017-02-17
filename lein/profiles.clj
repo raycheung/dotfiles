96e5224a80a0082eaedcb72d00f7fe3763b51f8b
Ray Cheung <ray.cheung@segantii.com>
2017-02-17 13:15:29 +0800

    Add lein-vanity

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-ancient "0.6.10"]
+                  [lein-vanity "0.2.0"]
                   [io.aviso/pretty "0.1.33"]]
         :dependencies [[io.aviso/pretty "0.1.33"]]}}
