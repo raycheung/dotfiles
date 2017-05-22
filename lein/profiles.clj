ead8708cff633bf4d11b0d09169c30ee8b02ddfc
Ray Cheung <ray.cheung@segantii.com>
2017-05-22 16:36:45 +0800

    Update eastwood

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:plugins [[io.aviso/pretty "0.1.33"]
-                  [jonase/eastwood "0.2.3"]
+                  [jonase/eastwood "0.2.4"]
                   [lein-kibit "0.1.3"]
                   [lein-cljfmt "0.5.6"]
                   [lein-ancient "0.6.10"]
